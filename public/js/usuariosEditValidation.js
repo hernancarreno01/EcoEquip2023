92c436fa92b1a5646dbcaae2764c7047a13abc0d
hernancarreno01 <radioportatil@gmail.com>
2023-11-25 00:17:11 -0300

    validacion front editar perfil

--- public/js/usuariosEditValidation.js
+++ public/js/usuariosEditValidation.js
@@ -1,43 +1,43 @@
 window.onload = function () {
-  let nombreUsuario = document.querySelector(".userRegisterTitle");
+  let nombreUsuario = document.querySelector(".userEditTitle");
   let formulario = document.querySelector("#formulario");
   let article = document.querySelector(".article");
   const tieneMayuscula = /[A-Z]/;
   const tieneNumero = /\d/;
 
 
-  const inputNombreUsuario = document.querySelector("#nombre_usuario");
+  const inputNombreUsuario = document.querySelector("#userName");
   inputNombreUsuario.focus();
   const form = document.querySelector(".signinForm");
   form.addEventListener("submit", (e) => {
     let errores = [];
 
     e.preventDefault();
-    if (form.nombre_usuario.value == "") {
-      form.nombre_usuario.classList.remove("is-valid");
-      form.nombre_usuario.classList.add("is-invalid");
+    if (form.userName.value == "") {
+      form.userName.classList.remove("is-valid");
+      form.userName.classList.add("is-invalid");
       errores.push("El campo nombre de usuario es obligatorio");
-    } else if (form.nombre_usuario.value.length < 5) {
-      form.nombre_usuario.classList.remove("is-valid");
-      form.nombre_usuario.classList.add("is-invalid");
+    } else if (form.userName.value.length < 5) {
+      form.userName.classList.remove("is-valid");
+      form.userName.classList.add("is-invalid");
       errores.push(
         "El campo nombre de usuario debe tener al menos 5 caracteres"
       );
     } else {
-      form.nombre_usuario.classList.remove("is-invalid");
-      form.nombre_usuario.classList.add("is-valid");
+      form.userName.classList.remove("is-invalid");
+      form.userName.classList.add("is-valid");
     }
-    if (form.nombres.value == "") {
-      form.nombres.classList.remove("is-valid");
-      form.nombres.classList.add("is-invalid");
+    if (form.firstName.value == "") {
+      form.firstName.classList.remove("is-valid");
+      form.firstName.classList.add("is-invalid");
       errores.push("El campo nombre es obligatorio");
-    } else if (form.nombres.value.length < 2) {
-      form.nombres.classList.remove("is-valid");
-      form.nombres.classList.add("is-invalid");
+    } else if (form.firstName.value.length < 2) {
+      form.firstName.classList.remove("is-valid");
+      form.firstName.classList.add("is-invalid");
       errores.push("El campo nombre debe tener al menos 2 caracteres");
     } else {
-      form.nombres.classList.remove("is-invalid");
-      form.nombres.classList.add("is-valid");
+      form.firstName.classList.remove("is-invalid");
+      form.firstName.classList.add("is-valid");
     }
     if (form.apellido.value == "") {
       form.apellido.classList.remove("is-valid");
@@ -72,34 +72,13 @@ window.onload = function () {
       form.email.classList.remove("is-invalid");
       form.email.classList.add("is-valid");
     }
-    if (form.direccion.value == "") {
-      form.direccion.classList.remove("is-valid");
-      form.direccion.classList.add("is-invalid");
-      errores.push("El campo direccion es obligatorio");
+    if (form.adress1.value == "") {
+      form.adress1.classList.remove("is-valid");
+      form.adress1.classList.add("is-invalid");
+      errores.push("El campo adress es obligatorio");
     } else {
-      form.direccion.classList.remove("is-invalid");
-      form.direccion.classList.add("is-valid");
-    }
-    if (form.contrasenia.value == "") {
-      form.contrasenia.classList.remove("is-valid");
-      form.contrasenia.classList.add("is-invalid");
-      errores.push("La contraseña es obligatoria");
-    } else if (form.contrasenia.value.length < 8) {
-      form.contrasenia.classList.remove("is-valid");
-      form.contrasenia.classList.add("is-invalid");
-      errores.push("La contraseña debe tener al menos 8 caracteres");
-    } else if (
-      !tieneMayuscula.test(form.contrasenia.value) ||
-      !tieneNumero.test(form.contrasenia.value)
-    ) {
-      form.contrasenia.classList.remove("is-valid");
-      form.contrasenia.classList.add("is-invalid");
-      errores.push(
-        "La contraseña debe contener al menos una letra mayúscula y un número"
-      );
-    } else {
-      form.contrasenia.classList.remove("is-invalid");
-      form.contrasenia.classList.add("is-valid");
+      form.adress1.classList.remove("is-invalid");
+      form.adress1.classList.add("is-valid");
     }
     if (form.telefono.value == "") {
       form.telefono.classList.remove("is-valid");
@@ -109,24 +88,24 @@ window.onload = function () {
       form.telefono.classList.remove("is-invalid");
       form.telefono.classList.add("is-valid");
     }
-    if (form.avatar.value == "") {
-      form.avatar.classList.remove("is-valid");
-      form.avatar.classList.add("is-invalid");
+    if (form.imagen_perfil.value == "") {
+      form.imagen_perfil.classList.remove("is-valid");
+      form.imagen_perfil.classList.add("is-invalid");
       errores.push("Debe cargar una imagen de usuario");
     } else {
-      const file = form.avatar.files[0];
+      const file = form.imagen_perfil.files[0];
       if (!/\.(jpg|jpeg|png|bmp)$/i.test(file.name)) {
-        form.avatar.classList.remove("is-valid");
-        form.avatar.classList.add("is-invalid");
+        form.imagen_perfil.classList.remove("is-valid");
+        form.imagen_perfil.classList.add("is-invalid");
         errores.push("El archivo debe ser de tipo jpg, jpeg, png o bmp");
       } else if (file.size > 5 * 1024 * 1024) {
         // Validar tamaño del archivo (5 MB)
-        form.avatar.classList.remove("is-valid");
-        form.avatar.classList.add("is-invalid");
+        form.imagen_perfil.classList.remove("is-valid");
+        form.imagen_perfil.classList.add("is-invalid");
         errores.push("El tamaño del archivo debe ser menor a 5 MB");
       } else {
-        form.avatar.classList.remove("is-invalid");
-        form.avatar.classList.add("is-valid");
+        form.imagen_perfil.classList.remove("is-invalid");
+        form.imagen_perfil.classList.add("is-valid");
       }
       const ul = document.querySelector(".errores");
       if (errores.length != 0) {
@@ -151,10 +130,10 @@ window.onload = function () {
         ).then(() => { form.submit(); })
       }
     }
-    const captNombres = document.getElementById("nombres");
+    const captNombres = document.getElementById("firstName");
     const captApellido = document.getElementById("apellido");
     const captEmail = document.getElementById("email");
-    const captDireccion = document.getElementById("direccion");
+    const captDireccion = document.getElementById("adress");
     const captCiudad = document.getElementById("ciudad_id");
     const CaptRol = document.getElementById("rol_id");
     const captContrasenia = document.getElementById("constrasenia");
